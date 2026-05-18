38ab417ca850fe6441319c9dccb7729db5d18abf
hernan.colomberat <herna14@gmail.com>
2026-05-18 12:09:10 -0300

    refactor(iaac-stack.ts): removed unneeded path function

--- lib/iaac-stack.ts
+++ lib/iaac-stack.ts
@@ -2,25 +2,26 @@ import * as cdk from 'aws-cdk-lib/core';
 import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import * as path from 'path';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class IaacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const handlerPath = 'lib/functions/handler.ts';
+
 
     // Lambda Function
     const newOrderFunction = new lambda.NodejsFunction(this, 'NewOrderFunction', {
       runtime: Runtime.NODEJS_24_X,
-      entry: path.join(__dirname, 'functions/handler.ts'),
+      entry: handlerPath,
       handler: 'newOrder',
     });
 
     // Lambda Function
     const getOrderFunction = new lambda.NodejsFunction(this, 'GetOrderFunction', {
       runtime: Runtime.NODEJS_24_X,
-      entry: path.join(__dirname, 'functions/handler.ts'),
+      entry: handlerPath,
       handler: 'getOrder',
     });
 
