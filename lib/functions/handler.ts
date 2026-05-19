bd2944ca27d472cc4d5afa8c0be4401965c8b164
hernan.colomberat <herna14@gmail.com>
2026-05-19 18:01:32 -0300

    feat(dynamodb): added orders table

--- lib/functions/handler.ts
+++ lib/functions/handler.ts
@@ -1,12 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, SQSEvent } from 'aws-lambda'
-import { SqsEventSourceProps } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { SQSClient, SendMessageCommand, SendMessageCommandInput } from '@aws-sdk/client-sqs';
+
 
 const newOrder = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const id = uuidv4();
   let orderPayload;
   try {
     orderPayload = JSON.parse(event.body || '{}');
+    await sendMessageToSqs({ id, ...orderPayload }, process.env.PENDING_ORDERS_QUEUE_URL as string);
   } catch (error) {
     console.log(error);
     return {
@@ -53,5 +55,39 @@ const prepOrder = async (event: SQSEvent) => {
   return;
 }
 
+const sendOrder = async (event) => {
 
-export { getOrder, newOrder, prepOrder };
+  const order = {
+    orderId: event.orderId,
+    tipo: event.tipo,
+    precio: event.precio
+  }
+  console.log(order);
+  await sendMessageToSqs(event, process.env.ORDERS_TO_SEND_QUEUE_URL)
+}
+
+const sendMessageToSqs = async (message: string, queueUrl: string) => {
+
+  if (!queueUrl) {
+    console.error('QUEUE_URL is not defined');
+    throw new Error('QUEUE_URL is not defined');
+  }
+
+  const client = new SQSClient({});
+  const command = new SendMessageCommand({
+    QueueUrl: queueUrl,
+    MessageBody: JSON.stringify(message),
+  });
+
+  try {
+    const data = await client.send(command);
+    console.log('Message sent to SQS:', data.MessageId);
+    return data;
+  } catch (error) {
+    console.error('Error sending message to SQS:', error);
+    throw error;
+  }
+}
+
+
+export { getOrder, newOrder, prepOrder, sendOrder };
--- lib/iaac-stack.ts
+++ lib/iaac-stack.ts
@@ -5,6 +5,7 @@ import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class IaacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,13 +14,19 @@ export class IaacStack extends cdk.Stack {
 
     // Sqs Queue
     const pendingOrdersQueue = new Queue(this, 'PendingOrdersQueue', {});
+    const ordersToSendQueue = new Queue(this, 'ToSendOrdersQueue', {});
 
     // Lambda Function
     const newOrderFunction = new lambda.NodejsFunction(this, 'NewOrderFunction', {
       runtime: Runtime.NODEJS_24_X,
       entry: handlerPath,
       handler: 'newOrder',
+      environment: {
+        PENDING_ORDERS_QUEUE_URL: pendingOrdersQueue.queueUrl
+      }
     });
+
+    pendingOrdersQueue.grantSendMessages(newOrderFunction)
 
     // Lambda Function
     const getOrderFunction = new lambda.NodejsFunction(this, 'GetOrderFunction', {
@@ -32,14 +39,34 @@ export class IaacStack extends cdk.Stack {
     const prepOrderFunction = new lambda.NodejsFunction(this, 'PrepOrderFunction', {
       runtime: Runtime.NODEJS_24_X,
       entry: handlerPath,
-      handler: 'prepOrder',
+      handler: 'prepOrder'
     });
+
+    // Lambda Function
+
+    const sendOrderFunction = new lambda.NodejsFunction(this, 'SendOrderFunction', {
+      runtime: Runtime.NODEJS_24_X,
+      entry: handlerPath,
+      handler: 'sendOrder',
+      environment: {
+        ORDERS_TO_SEND_QUEUE_URL: ordersToSendQueue.queueUrl
+      }
+    })
+
+    ordersToSendQueue.grantSendMessages(sendOrderFunction);
 
     // Event Sourcing
     prepOrderFunction.addEventSource(new SqsEventSource(pendingOrdersQueue, {
       batchSize: 1 // Ejecuta llamada a función por cada mensaje en la queue
-    }))
+    }));
 
+    // DynamoDB
+    const ordersTable = new Table(this, 'OrdersTable', {
+      billingMode: BillingMode.PROVISIONED,
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      readCapacity: 2,
+      writeCapacity: 1
+    })
 
     // Api Gateway Service
     const api = new apigateway.RestApi(this, 'OrderApi', {
